f57e015dd3798aac4630556e7ae7b139065bbebe
Essadike  Elhafiane <eelhafia@e1r8p6.1337.ma>
2023-11-14 15:20:32 +0100

    last push

---
+++ srcs/requirements/mariadb/tools/conf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+service mariadb start;
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mariadb -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
+mysqld_safe
