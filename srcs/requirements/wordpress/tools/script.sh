9e61f3f075fd7232966526dd4b2a021c771c7d56
Essadike  Elhafiane <eelhafia@e1r8p6.1337.ma>
2023-11-16 20:49:01 +0100

    last push

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,22 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "/etc/php/7.4/fpm/pool.d/www.conf";
-chown -R www-data:www-data /var/www/*;
-chown -R 755 /var/www/*;
-mkdir -p /run/php/;
-touch /run/php/php7.4-fpm.pid;
-if [ ! -f /var/www/html/wp-config.php ]; then
-  echo "Wordpress: setting up..."
-  cd /var/www/html
-  wp core download --allow-root
-  wp config create 
-  mv /var/www/wp-config.php /var/www/html/
-  echo "Wordpress: creating users..."
-  wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-  wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD}
-  wp plugin install redis-cache --activate --allow-root
-  wp plugin update --all --allow-root
-  wp plugin activate redis-cache --allow-root
-  wp redis enable --force --allow-root
+sleep 3;
+if [ -f ./wp-config.php ]
+then
+	echo "Wordpress already downloaded"
+else
+	mv /var/www/wp-config.php /var/www/html
+	wp core download --allow-root
+	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD}
+	wp plugin install redis-cache --activate --allow-root
+	wp plugin update --all --allow-root
+	wp plugin activate redis-cache --allow-root
+	wp redis enable --force --allow-root
 fi
+
 exec "$@"
