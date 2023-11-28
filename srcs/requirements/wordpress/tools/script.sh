3e93a8317a67d07e520b437206b43ff9470886d3
Essadike  Elhafiane <eelhafia@e1r8p6.1337.ma>
2023-11-28 15:40:49 +0100

    last push

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,7 +8,7 @@ else
     tar -xvzf latest.tar.gz --strip-components=1 && \
     rm latest.tar.gz
 	wp core download --allow-root
-	wp config create --allow-root --dbname=${MARIADB_DATABASE} --dbuser=${WP_ADMIN_LOGIN} --dbpass=${WP_ADMIN_PASSWORD} --dbhost=mariadb:3306 --path='/var/www/html'
+	wp config create --allow-root --dbname=${MARIADB_DATABASE} --dbuser=${WP_ADMIN_LOGIN} --dbpass=${WP_ADMIN_PASSWORD} --dbhost=${DB_HOST} --path='/var/www/html'
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD}
 
